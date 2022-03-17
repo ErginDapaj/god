12d7ad99b967342263cf88c46f087efbc1c3e4fb
Grainger <39377884+remixor@users.noreply.github.com>
2022-03-17 12:37:14 +0100

    Fixd

---
+++ God.js
@@ -0,0 +1,19 @@
+let God = require(god)
+let sonOfGod = require(jesus)
+
+if (God.dies){
+    sonOfGod.revive()
+}
+
+if (somethingBadHappens){
+    prayTo(God)
+}
+
+if (somethingGoodHappens){
+    itWas(God)
+}
+
+if (someoneIsPregnant) {
+    itWas(God)
+}
+
---
+++ parentsOfGod.js
@@ -0,0 +1,9 @@
+let father = require(unknown)
+let mother = require(unknown)
+
+//That's it 
+if (question("Who made god?")){
+    return answer("No one, he is the creator")
+} else if(question("Who made the creator")){
+    return answer("Shut the fuck up already mate!")
+}
